5404fb9aabab8b51bb026f4d9a882f348c0c2423
Neil Lasrado <neil@digithinkit.com>
2018-08-07 16:26:45 +0530

    Added icons

--- mdio/config/desktop.py
+++ mdio/config/desktop.py
@@ -3,12 +3,22 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-	return [
-		{
-			"module_name": "MDIO Pranali",
-			"color": "RED",
-			"icon": "octicon octicon-broadcast",
-			"type": "module",
-			"label": _("MDIO Pranali")
+	return {
+		"District": {
+			"color": "#9b59b6",
+			"icon": "icon-globe",
+			"icon": "octicon octicon-globe",
+			"link": "List/District",
+			"doctype": "District",
+			"type": "list"
+		},
+		
+		"Project": {
+			"color": "#c23c59",
+			"icon": "octicon octicon-rocket",
+			"label": _("Project"),
+			"link": "List/Project",
+			"doctype": "Project",
+			"type": "list"
 		}
-	]
+	}
